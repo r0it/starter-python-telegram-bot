730f028cc41e582ccfade8ab30601af4174ce77d
Rohit Shinde <rohitshinde1989@gmail.com>
2024-02-09 13:19:32 +0530

    fixed

--- main.py
+++ main.py
@@ -55,24 +55,26 @@ logging.basicConfig(
 
 async def start(update: Update, context: CustomContext) -> None:
     """Display a message with instructions on how to use this bot."""
-    payload_url = html.escape(f"{URL}/submitpayload?user_id=<your user id>&payload=<payload>")
-    text = (
-        f"To check if the bot is still running, call <code>{URL}/healthcheck</code>.\n\n"
-        f"To post a custom update, call <code>{payload_url}</code>."
-    )
-    await update.message.reply_html(text=text)
+    # payload_url = html.escape(f"{URL}/submitpayload?user_id=<your user id>&payload=<payload>")
+    # text = (
+    #     f"To check if the bot is still running, call <code>{URL}/healthcheck</code>.\n\n"
+    #     f"To post a custom update, call <code>{payload_url}</code>."
+    # )
+    # await update.message.reply_html(text=text)
+    await update.message.reply_text('Hi, this is a test')
 
 async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
     """Handle custom updates."""
-    chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
-    payloads = context.user_data.setdefault("payloads", [])
-    payloads.append(update.payload)
-    combined_payloads = "</code>\n• <code>".join(payloads)
-    text = (
-        f"The user {chat_member.user.mention_html()} has sent a new payload. "
-        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
-    )
-    await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
+    # chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
+    # payloads = context.user_data.setdefault("payloads", [])
+    # payloads.append(update.payload)
+    # combined_payloads = "</code>\n• <code>".join(payloads)
+    # text = (
+    #     f"The user {chat_member.user.mention_html()} has sent a new payload. "
+    #     f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
+    # )
+    # await context.bot.send_message(chat_id=update.user_id, text=text, parse_mode=ParseMode.HTML)
+    await update.message.reply_text('Hi, this is a custom test')
 
 
 # Load variables from .env file if present
