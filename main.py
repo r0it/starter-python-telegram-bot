8d3c8e3508fc3a9172b79638eea230d389db664d
Rohit Shinde <rohitshinde1989@gmail.com>
2024-02-09 10:18:53 +0530

    retest

--- main.py
+++ main.py
@@ -16,25 +16,24 @@ load_dotenv()
 # Read the variable from the environment (or .env file)
 bot_token = os.getenv('BOT_TOKEN')
 secret_token = os.getenv("SECRET_TOKEN")
-# webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
+webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
 
 bot = Bot(token=bot_token)
-# bot.set_webhook(url=webhook_url)
-# webhook_info = bot.get_webhook_info()
-# print(webhook_info)
+bot.set_webhook(url=webhook_url)
+webhook_info = bot.get_webhook_info()
+print(webhook_info)
 
 def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> str:
     # return true # uncomment to disable authentication
-    # if x_telegram_bot_api_secret_token != secret_token:
-    # raise HTTPException(status_code=403, detail="Not authenticated")
-    print(secret_token)
-    return secret_token
+    if x_telegram_bot_api_secret_token != secret_token:
+        raise HTTPException(status_code=403, detail="Not authenticated")
+    return x_telegram_bot_api_secret_token
 
 @app.post("/webhook/")
 async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
     chat_id = update.message["chat"]["id"]
     text = update.message["text"]
-    # print("Received message:", update.message)
+    print("Received message:", update.message)
 
     if text == "/start":
         with open('hello.gif', 'rb') as photo:
@@ -44,3 +43,4 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
 
     return {"ok": True}
+
