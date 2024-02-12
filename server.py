a1046f6c54089305115fc8fad5fbc2fb8ed03d55
Rohit Shinde <rohitshinde1989@gmail.com>
2024-02-12 23:28:13 +0530

    local-fixed

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from main import app, ptb
 # Use polling when running locally
 if __name__ == "__main__":
     if not config.ENV:
-        print(f"Application started without webhook: {config.CYCLIC_URL}")
+        print(f"Application started without webhook: {config.WEB_HOST}")
         ptb.run_polling(poll_interval=3)
     else:
         # Used for testing webhook locally, instructions for how to set up local webhook at https://dev.to/ibrarturi/how-to-test-webhooks-on-your-localhost-3b4f
